c63558ad8dd66b7e28e5bc0077ef0a0c6548172a
Dell <Dell@DESKTOP-EFOD4LI>
2023-11-03 10:54:24 +0530

    updted name

--- src/githubProjectFirst/Shyam1.java
+++ src/githubProjectFirst/Shyam1.java
@@ -3,8 +3,8 @@ package githubProjectFirst;
 public class Shyam1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Hi this is Kiran");
 	}
 
 }
